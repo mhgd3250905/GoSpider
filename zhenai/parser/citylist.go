44e6fc626fdaf44dd3968e7b417e7eb4c5c9354d
ksheng <294851575@qq.com>
2018-03-25 14:18:28 +0800

    1.完成第一个单任务爬虫

---
+++ zhenai/parser/citylist.go
@@ -0,0 +1,23 @@
+package parser
+
+import (
+	"GoSpider/engine"
+	"regexp"
+)
+
+const cityListRe = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`
+
+func ParseCityList(contents []byte) engine.ParseResult {
+	re := regexp.MustCompile(cityListRe)
+	matchs := re.FindAllSubmatch(contents, -1)
+
+	result := engine.ParseResult{}
+	for _, m := range matchs {
+		result.Items = append(result.Items, m[2])
+		result.Requests = append(result.Requests, engine.Request{
+			string(m[1]),
+			engine.NilParser,
+		})
+	}
+	return result
+}
