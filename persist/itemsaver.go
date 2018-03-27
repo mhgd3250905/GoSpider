cf7461215239886da6c99af8b7934e516f4bec92
ksheng <294851575@qq.com>
2018-03-28 01:21:27 +0800

    开始保存

--- persist/itemsaver.go
+++ persist/itemsaver.go
@@ -1,6 +1,10 @@
 package persist
 
-import "log"
+import (
+	"log"
+	"github.com/olivere/elastic"
+	"context"
+)
 
 func ItemSaver() chan interface{} {
 	out := make(chan interface{})
@@ -10,7 +14,31 @@ func ItemSaver() chan interface{} {
 			item := <-out
 			log.Printf("Item Saver: Got item #%d: %v", itemCount, item)
 			itemCount++
+
+			save(item)
 		}
 	}()
 	return out
+}
+
+//save item
+func save(item interface{}) (id string, err error) {
+	client, err := elastic.NewClient()
+	if err != nil {
+		return "", err
+	}
+
+	//save data -> index
+	resp, err := client.Index().
+		Index("dating_profile").
+		Type("zhenai").
+		BodyJson(item).
+		Do(context.Background())
+
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Id, nil
+
 }
---
+++ persist/itemsaver_test.go
@@ -0,0 +1,51 @@
+package persist
+
+import (
+	"testing"
+	"GoSpider/modle"
+	"github.com/olivere/elastic"
+	"context"
+	"log"
+	"encoding/json"
+)
+
+func TestSave(t *testing.T) {
+	profile := modle.Profile{
+		Age:        34,
+		Height:     162,
+		Weight:     57,
+		Income:     "3001-5000元",
+		Gender:     "女",
+		Name:       "安静的雪",
+		Xinzuo:     "牡羊座",
+		Occupation: "人事/行政",
+		Marriage:   "离异",
+		House:      "已购房",
+		Hukou:      "山东菏泽",
+		Education:  "大学本科",
+		Car:        "未购车",
+	}
+
+	id, err := save(profile)
+	if err != nil {
+		panic(err)
+	}
+
+	client, err := elastic.NewClient()
+	if err != nil {
+		panic(err)
+	}
+
+	resp,err:=client.Get().
+		Index("dating_profile").
+		Type("zhenai").
+		Id(id).
+		Do(context.Background())
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("%s",resp.Source)
+
+}
