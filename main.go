3cda7cc7baa2bdcf2cdbd2ee55102da6793e3d7a
ksheng <294851575@qq.com>
2018-04-03 23:12:16 +0800

    1更新知乎日报内容

--- main.go
+++ main.go
@@ -4,52 +4,53 @@ import (
 	"GoSpider/engine"
 	"GoSpider/scheduler"
 	"GoSpider/persist"
-	"GoSpider/huxiu/parser"
-	"time"
+	"GoSpider/zhihu/parser"
 )
 
 func main() {
-	for {
-		itemChan, err := persist.ItemSaverRedis("dating_profile_2")
-		if err != nil {
-			panic(err)
-		}
-		e := engine.ConcurrentEngine{
-			Scheduler:   &scheduler.QueueScheduler{},
-			WorkerCount: 20,
-			ItemChan:    itemChan,
-			Header: map[string]string{
-				"User-Agent": `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36`,
-				"Cookie":     `screen=%7B%22w%22%3A1536%2C%22h%22%3A864%2C%22d%22%3A1.25%7D; aliyungf_tc=AQAAAOuQyXg3NA0A40KttGHnrOtzsJK1; screen=%7B%22w%22%3A1536%2C%22h%22%3A864%2C%22d%22%3A1.25%7D; SERVERID=03a07aad3597ca2bb83bc3f5ca3decf7|1522469603|1522469119`,
-			},
-		}
+	//for {
+	//	go func() {
+	//		itemChan, err := persist.ItemSaverRedis("dating_profile_2")
+	//		if err != nil {
+	//			panic(err)
+	//		}
+	//		e := engine.ConcurrentEngine{
+	//			Scheduler:   &scheduler.QueueScheduler{},
+	//			WorkerCount: 20,
+	//			ItemChan:    itemChan,
+	//			Header: map[string]string{
+	//				"User-Agent": `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36`,
+	//				"Cookie":     `screen=%7B%22w%22%3A1536%2C%22h%22%3A864%2C%22d%22%3A1.25%7D; aliyungf_tc=AQAAAOuQyXg3NA0A40KttGHnrOtzsJK1; screen=%7B%22w%22%3A1536%2C%22h%22%3A864%2C%22d%22%3A1.25%7D; SERVERID=03a07aad3597ca2bb83bc3f5ca3decf7|1522469603|1522469119`,
+	//			},
+	//		}
+	//
+	//		e.Run(engine.Request{
+	//			"https://www.huxiu.com/",
+	//			parser.ParseColumnList,
+	//		})
+	//	}()
+	//
+	//	<-time.After(time.Minute * 30)
+	//}
 
-		e.Run(engine.Request{
-			"https://www.huxiu.com/",
-			parser.ParseColumnList,
-		})
-		<-time.After(time.Minute * 30)
-
+	itemChan, err := persist.ItemSaverRedis("dating_profile_2")
+	if err != nil {
+		panic(err)
+	}
+	e := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.QueueScheduler{},
+		WorkerCount: 20,
+		ItemChan:    itemChan,
+		Header: map[string]string{
+			"User-Agent": `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36`,
+			"Cookie":     `screen=%7B%22w%22%3A1536%2C%22h%22%3A864%2C%22d%22%3A1.25%7D; aliyungf_tc=AQAAAOuQyXg3NA0A40KttGHnrOtzsJK1; screen=%7B%22w%22%3A1536%2C%22h%22%3A864%2C%22d%22%3A1.25%7D; SERVERID=03a07aad3597ca2bb83bc3f5ca3decf7|1522469603|1522469119`,
+		},
 	}
 
-	//itemChan, err := persist.ItemSaverRedis("dating_profile_3")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//e := engine.ConcurrentEngine{
-	//	Scheduler:   &scheduler.QueueScheduler{},
-	//	WorkerCount: 20,
-	//	ItemChan:    itemChan,
-	//	Header: map[string]string{
-	//		"User-Agent": `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36`,
-	//		"Cookie":`screen=%7B%22w%22%3A1536%2C%22h%22%3A864%2C%22d%22%3A1.25%7D; aliyungf_tc=AQAAAOuQyXg3NA0A40KttGHnrOtzsJK1; screen=%7B%22w%22%3A1536%2C%22h%22%3A864%2C%22d%22%3A1.25%7D; SERVERID=03a07aad3597ca2bb83bc3f5ca3decf7|1522469603|1522469119`,
-	//	},
-	//}
-	//
-	//e.Run(engine.Request{
-	//	"https://daily.zhihu.com/",
-	//	parser.ParseZhihuHome,
-	//})
+	e.Run(engine.Request{
+		"https://daily.zhihu.com/",
+		parser.ParseZhihuHome,
+	})
 
 	//仅仅爬取上海
 	//e.Run(engine.Request{
--- zhihu/parser/home.go
+++ zhihu/parser/home.go
@@ -11,17 +11,24 @@ import (
 2->imgUrl
 3->title
 */
-var itemRe = regexp.MustCompile(`<div class="wrap">
-                        <div class="box"><a href="(/story/[^d]+)" class="link-button"><img
-                                src="([^"]+)"
-                                class="preview-image"><span class="title">（[^<]+）</span></a></div>
-                    </div>`)
+var itemRe = regexp.MustCompile(`<div class="wrap"><div class="box"><a href="(/story/[\d]+)" class="link-button"><img src="([^"]+)" class="preview-image"><span class="title">([^<]+)</span></a></div></div>`)
 
-var idUrlRe = regexp.MustCompile(`/story/（[^d]+）`)
+/*
+<div class="wrap">
+                        <div class="box"><a href="/story/9676894" class="link-button"><img
+                                src="https://pic3.zhimg.com/v2-8b2c4ce03c630231618f7845354ebfca.jpg"
+                                class="preview-image"><span class="title">武汉理工大学研究生自杀，「天然畸形」的高校师生关系脱不了干系</span></a>
+                        </div>
+                    </div>
 
-var HOST = `https://daily.zhihu.com/`
+*/
+
+var idUrlRe = regexp.MustCompile(`/story/([^d]+)`)
+
+var HOST = `https://daily.zhihu.com`
 
 func ParseZhihuHome(contents []byte, _ string) engine.ParseResult {
+
 	matchs := itemRe.FindAllSubmatch(contents, -1)
 
 	var items []engine.Item
@@ -35,7 +42,7 @@ func ParseZhihuHome(contents []byte, _ string) engine.ParseResult {
 		//fmt.Printf("block:%s ,title:%s ,url:%s ,column:%s\n",matchs[i],new.Title,new.Url,column)
 
 		items = append(items, engine.Item{
-			Type:    "huxiu",
+			Type:    "zhihu",
 			Id:      extractString([]byte(new.Url), idUrlRe),
 			Payload: new,
 		})
